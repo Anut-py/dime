03b44b3f33beb8040b4435734dde0cf5af3e6534
Anand Swaroop <anandswaroop191@gmail.com>
2022-07-06 10:02:06 -0400

    Added code coverage settings

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/out/"]
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/out/"],
+  testResultsProcessor: "jest-sonar-reporter",
+  collectCoverage: true,
+  testResultsProcessor: "jest-sonar-reporter",
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/([^/]*/)*[^\\.]*.spec.ts"
+  ]
 };
